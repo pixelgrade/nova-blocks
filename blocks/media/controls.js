690993380e619b21c119299210308092c4ac5d3f
George <george.olaru@pixelgrade.com>
2019-06-14 16:33:14 +0300

    add tooltips labels to "Position" buttons from Toolbar #6

--- blocks/media/controls.js
+++ blocks/media/controls.js
@@ -24,13 +24,13 @@ class Controls extends Component {
 			<BlockControls>
 				<Toolbar>
 					<IconButton
-						title = {__('Show media on left', '__plugin_txtd')}
+						label = {__('Show Media on Left Side', '__plugin_txtd')}
 						icon = 'align-pull-left'
 						isActive= { mediaPosition === 'left' }
 						onClick={ () => setAttributes({ mediaPosition: 'left' }) }
 					/>
 					<IconButton
-						title = {__('Show media on right', '__plugin_txtd')}
+						label = {__('Show Media on Right Side', '__plugin_txtd')}
 						icon = 'align-pull-right'
 						isActive = { mediaPosition === 'right' }
 						onClick = { () => setAttributes({ mediaPosition: 'right' }) }
