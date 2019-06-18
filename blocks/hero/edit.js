f55f82e2be19a77994f3e2afb797c350ef9f037d
Razvan <razwan.me@gmail.com>
2019-06-18 16:32:00 +0300

    default blocks in hero

--- blocks/hero/edit.js
+++ blocks/hero/edit.js
@@ -231,7 +231,7 @@ export default class Edit extends Component {
 					<div className='nova-u-content-align'>
 						<div className='nova-hero__content nova-u-content-width' style={ styles.content }>
 							<InnerBlocks template={[
-								[ 'core/heading', { content: 'This is a catchy title', align: 'center', level: 2 } ],
+								[ 'core/heading', { content: 'This is a catchy title', align: 'center', level: 1 } ],
 								[ 'core/paragraph', { content: 'A brilliant subtitle to explain its catchiness', align: 'center' } ],
 								[ 'core/button', { text: 'Discover more', align: 'center' } ],
 							]} />
