ce8b98cb7fb395c94a8e6e6fdbffc03fa56e8e0d
Razvan <razwan.me@gmail.com>
2019-08-05 11:05:55 +0300

    add site title and description to logo block

--- src/blocks/logo/init.php
+++ src/blocks/logo/init.php
@@ -27,11 +27,34 @@ if ( ! function_exists( 'novablocks_render_logo_block' ) ) {
 
 		ob_start();
 
-		do_action( 'nova_logo:before' );
+		do_action( 'nova_logo:before' ); ?>
 
-		the_site_logo();
+		<div class="site-branding">
 
-		do_action( 'nova_logo:after' );
+		<?php if ( has_custom_logo() ) : ?>
+			<div class="site-logo"><?php the_custom_logo(); ?></div>
+		<?php endif; ?>
+		<?php $blog_info = get_bloginfo( 'name' ); ?>
+		<?php if ( ! empty( $blog_info ) ) : ?>
+			<?php if ( is_front_page() && is_home() ) : ?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+			<?php else : ?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+			<?php endif; ?>
+		<?php endif; ?>
+
+		<?php
+		$description = get_bloginfo( 'description', 'display' );
+		if ( $description || is_customize_preview() ) :
+			?>
+				<p class="site-description">
+					<?php echo $description; ?>
+				</p>
+		<?php endif; ?>
+
+		</div>
+
+		<?php do_action( 'nova_logo:after' );
 
 		return ob_get_clean();
 	}
