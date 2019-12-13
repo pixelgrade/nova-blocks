5381a37d9bdb4638de249e9ad23b31d6f5add633
madalingorbanescu <madalin@pixelgrade.com>
2019-12-13 16:29:31 +0200

    Fix scroll and position indicators on latest version of Gutenberg to close #57

--- nova-blocks.php
+++ nova-blocks.php
@@ -26,10 +26,16 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 function novablocks_plugin_setup() {
+	global $wp_version;
+	$is_old_wp_version = version_compare( $wp_version, '5.3', '<' );
+	$is_post_meta_broken = defined( 'GUTENBERG_VERSION' ) &&
+	                       version_compare( GUTENBERG_VERSION, '6.6.0', '>=' ) &&
+	                       version_compare( GUTENBERG_VERSION, '6.7.0', '<=' );
+
 	// Set up a constant that will tell the rest of the plugin whether to use post meta attributes or not.
 	// This is needed since Gutenberg 6.6.0 introduced an issue with them causing a crash in the editor.
 	if ( ! defined( 'NOVABLOCKS_USE_POST_META_ATTRIBUTES' ) ) {
-		if ( defined( 'GUTENBERG_VERSION' ) && version_compare( GUTENBERG_VERSION, '6.6.0', '>=' ) ) {
+		if ( $is_old_wp_version || $is_post_meta_broken ) {
 			define( 'NOVABLOCKS_USE_POST_META_ATTRIBUTES', false );
 		} else {
 			define( 'NOVABLOCKS_USE_POST_META_ATTRIBUTES', true );
