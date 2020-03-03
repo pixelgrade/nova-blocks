96e9db44a57e5ee587ce6899cfdc25259cd11342
Razvan <razwan.me@gmail.com>
2020-03-03 12:06:41 +0200

    mask advanced gallery image using CSS mask-image

--- src/components/advanced-gallery/preview.js
+++ src/components/advanced-gallery/preview.js
@@ -6,6 +6,8 @@ import { generatePath } from '../../components/blob/util';
 
 import AdvancedGallery from "./index";
 
+const { Fragment } = wp.element;
+
 const AdvancedGalleryPreview = ( props ) => {
 
 	const { images, ...attributes } = props.attributes;
@@ -38,14 +40,32 @@ const AdvancedGalleryItem = ( { gridItem, index, props } ) => {
 	} = props;
 
 	const clipPathId = `blob-preset-${ preset + index }-complexity-${ complexity }-smoothness-${ smoothness }`;
-	const gridItemImageStyle = gridItem.getImageStyle();
-	const blobClipStyle = { clipPath: `url(#${clipPathId})` };
-	const imageStyle = { ...gridItemImageStyle, ...blobClipStyle };
 
 	return (
-		<div className={ `novablocks-advanced-gallery__grid-item` } style={ gridItem.getStyle() }>
-			<img className={ `novablocks-advanced-gallery__image` } style={ imageStyle } src={ gridItem.image.url } hidden />
-			<svg className={ `novablocks-advanced-gallery__image` } viewBox={ '0 0 20 20' }>
+		<Fragment>
+			<Spring to={ { path: generatePath( preset, complexity, smoothness, index ) } }>
+				{ props => {
+
+					const svgString = `<svg viewBox='0 0 20 20' preserveAspectRatio='none' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' version='1.1'><path d='${ props.path }'></path></svg>`;
+					const svgBase64 = btoa(svgString);
+					const svgDataURI = `url("data:image/svg+xml;utf8,${svgString}")`;
+
+					const gridItemStyle = {
+						...gridItem.getStyle(),
+						maskImage: svgDataURI,
+						maskSize: '100% 100%',
+						WebkitMaskImage: svgDataURI,
+						WebkitMaskSize: '100% 100%',
+					}
+
+					return (
+						<animated.div className={ `novablocks-advanced-gallery__grid-item` } style={ gridItemStyle }>
+							<img className={ `novablocks-advanced-gallery__image` } style={ gridItem.getImageStyle() } src={ gridItem.image.url } />
+						</animated.div>
+					);
+				} }
+			</Spring>
+			{ false && <svg className={ `novablocks-advanced-gallery__image` } viewBox={ '0 0 20 20' }>
 				<defs>
 					<clipPath id={ clipPathId }>
 						<Spring to={ { path: generatePath( preset, complexity, smoothness, index ) } }>
@@ -54,8 +74,8 @@ const AdvancedGalleryItem = ( { gridItem, index, props } ) => {
 					</clipPath>
 				</defs>
 				<image preserveAspectRatio="xMidYMid slice" clipPath={ `url(#${ clipPathId })` } height="20" width="20" xlinkHref={ gridItem.image.url } />
-			</svg>
-		</div>
+			</svg> }
+		</Fragment>
 	);
 }
 
