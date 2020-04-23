46721e2576e3f704c52953537db725a601c0bc87
Razvan <razwan.me@gmail.com>
2020-04-23 12:14:54 +0300

    Fix Posts Collection Block render

--- lib/extras.php
+++ lib/extras.php
@@ -1062,10 +1062,10 @@ function novablocks_get_card_media_padding_top( $containerHeight ) {
 if ( ! function_exists( 'novablocks_get_collection_output' ) ) {
 
 	function novablocks_get_collection_output( $attributes, $content ) {
-		$classes = array( 'novablocks-cards-collection' );
+		$classes = array( 'novablocks-collection' );
 		$classes[] = 'novablocks-block';
 		$classes[] = 'alignfull';
-		$classes[] = 'novablocks-cards-collection--align-' . $attributes[ 'contentAlign' ];
+		$classes[] = 'novablocks-collection--align-' . $attributes[ 'contentAlign' ];
 
 		if ( ! empty( $attributes['className'] ) ) {
 			$classes[] = $attributes['className'];
@@ -1099,13 +1099,13 @@ if ( ! function_exists( 'novablocks_get_collection_output' ) ) {
 		<div class="<?php echo $className; ?>" style="<?php echo $style; ?>">
 			<div class="wp-block-group__inner-container">
 				<?php if ( ! empty( $attributes['showCollectionTitle'] ) ) {
-					echo '<' . $titleTag . ' class="novablocks-cards-collection__title">' . $attributes['title'] . '</' . $titleTag . '>';
+					echo '<' . $titleTag . ' class="novablocks-collection__title">' . $attributes['title'] . '</' . $titleTag . '>';
 				}
 				if ( ! empty( $attributes['showCollectionSubtitle'] ) ) { ?>
-					<p class="novablocks-cards-collection__subtitle is-style-lead"><?php echo $attributes['subtitle']; ?></p>
+					<p class="novablocks-collection__subtitle is-style-lead"><?php echo $attributes['subtitle']; ?></p>
 				<?php } ?>
-				<div class="novablocks-cards-collection__cards wp-block alignwide">
-					<div class="novablocks-cards-collection__layout">
+				<div class="novablocks-collection__cards wp-block alignwide">
+					<div class="novablocks-collection__layout">
 						<?php echo $content; ?>
 					</div>
 				</div>
@@ -1114,4 +1114,26 @@ if ( ! function_exists( 'novablocks_get_collection_output' ) ) {
 
 		<?php return ob_get_clean();
 	}
+}
+
+function novablocks_get_card_media_markup( $url ) {
+
+	ob_start(); ?>
+
+	<div class="novablocks-card__media-wrap">
+		<div class="novablocks-card__media">
+			<?php if ( ! empty( $url ) ) { ?>
+				<img class="novablocks-card__media-image" src="<?php echo $url ?>" />
+			<?php } else { ?>
+				<div class="novablocks-card__media-placeholder">
+					<svg width="100" height="67" viewBox="0 0 100 67" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M96.722 0H3.279C1.229 0 0 1.229 0 3.279V63.115C0 65.164 1.229 66.393 3.279 66.393H96.721C98.771 66.393 99.999 65.164 99.999 63.115V3.279C100 1.229 98.771 0 96.722 0ZM4.918 6.558C4.918 5.533 5.532 4.918 6.557 4.918H93.443C94.468 4.918 95.082 5.533 95.082 6.558V59.836C95.082 60.08 95.045 60.3 94.978 60.495C88.865 54.214 68.521 33.606 64.755 33.606C60.757 33.606 39.42 56.811 35.172 61.475H31.447C33.415 59.153 36.274 55.808 39.525 52.107C34.42 47.976 29.403 44.263 27.87 44.263C25.059 44.263 11.092 56.738 5.979 61.391C5.309 61.196 4.919 60.648 4.919 59.836V6.558H4.918Z" fill="#323067"/>
+						<path d="M38.119 16.629C42.731 16.629 46.471 20.366 46.471 24.978C46.471 29.59 42.731 33.328 38.119 33.328C33.508 33.328 29.768 29.59 29.768 24.978C29.769 20.367 33.508 16.629 38.119 16.629Z" fill="#323067"/>
+					</svg>
+				</div>
+			<?php } ?>
+		</div>
+	</div>
+
+	<?php return ob_get_clean();
 }
--- src/blocks/posts-collection/init.php
+++ src/blocks/posts-collection/init.php
@@ -23,6 +23,89 @@ if ( ! function_exists( 'novablocks_render_posts_collection_block' ) ) {
 		$attributes_config = novablocks_get_posts_collection_attributes();
 		$attributes = novablocks_get_attributes_with_defaults( $attributes, $attributes_config );
 
-		return novablocks_get_collection_output( $attributes, '' );
+		$args = array(
+			'posts_per_page'   => $attributes['numberOfPosts'],
+			'post_status'      => 'publish',
+			'order'            => $attributes['order'],
+			'orderby'          => $attributes['orderBy'],
+			'suppress_filters' => false,
+			'post__not_in'     => array( get_the_ID() ),
+		);
+
+		if ( isset( $attributes['categories'] ) ) {
+			$args['category'] = $attributes['categories'];
+		}
+
+		$posts = get_posts( $args );
+
+		ob_start();
+
+		foreach ( $posts as $post ) {
+			$card_markup = novablocks_get_post_card_markup( $post, $attributes );
+			echo apply_filters( 'novablocks_post_card_markup', $card_markup, $post, $attributes );
+		}
+
+		$content = ob_get_clean();
+		$content = apply_filters( 'novablocks_posts_collection_markup', $content, $attributes );
+
+		return novablocks_get_collection_output( $attributes, $content );
 	}
+}
+
+function novablocks_get_post_card_markup( $post, $attributes ) {
+
+	$media = get_the_post_thumbnail_url( $post );
+	$date = esc_attr( get_the_date( 'c', $post ) );
+	$dateReadable = esc_html( get_the_date( '', $post ) );
+	$title = get_the_title( $post );
+	$postLink = esc_url( get_permalink( $post ) );
+	$excerpt = $post->post_excerpt;
+
+	if ( ! ( $excerpt ) ) {
+		$excerpt = $post->post_content;
+	}
+
+	$hlevel = $attributes['level'];
+	$titleTag = 'h' . ( $hlevel + 1 );
+	$subtitleTag = 'h' . ( $hlevel + 2 );
+
+	ob_start(); ?>
+
+	<div class="novablocks-card novablocks-block__content novablocks-card__inner-container">
+
+		<?php
+
+		if ( false != $attributes['showMedia'] ) {
+			echo novablocks_get_card_media_markup( $media, $attributes );
+		}
+
+		if ( false != $attributes['showMeta'] && ! empty( $dateReadable ) ) { ?>
+			<div class="novablocks-card__meta is-style-meta"><?php echo $dateReadable; ?></div>
+		<?php }
+
+		if ( false != $attributes['showTitle'] && ! empty( $title ) ) {
+			echo '<' . $titleTag . ' class="novablocks-card__title">' . $title . '</' . $titleTag . '>';
+		}
+
+		if ( false != $attributes['showSubtitle'] ) {
+			echo '<' . $subtitleTag . ' class="novablocks-card__subtitle">' . 'Category' . '</' . $subtitleTag . '>';
+		}
+
+		if ( false != $attributes['showDescription'] && ! empty( $excerpt ) ) { ?>
+			<div class="novablocks-card__description"><?php echo $excerpt; ?></div>
+		<?php } ?>
+
+		<?php if ( false != $attributes['showButtons'] ) { ?>
+
+			<div class="novablocks-card__buttons">
+				<div class="wp-block-button">
+					<a href="<?php echo get_permalink( $post ); ?>" class="wp-block-button__link">Read More</a>
+				</div>
+			</div>
+
+		<?php } ?>
+
+	</div>
+
+	<?php return ob_get_clean();
 }
