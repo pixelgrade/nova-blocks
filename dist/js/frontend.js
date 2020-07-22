4f5e9317c7df4f9a91ad1521b0f01f9a4fa058fc
Razvan <razwan.me@gmail.com>
2020-07-22 12:23:57 +0300

    Add utm parameters with app name for unsplash links

--- dist/js/frontend.js
+++ dist/js/frontend.js
@@ -21767,7 +21767,9 @@ var unsplash_default = /*#__PURE__*/__webpack_require__.n(unsplash);
 
 
 
+var APP_NAME = 'Nova Blocks';
 var COLLECTION_ID = 10606015;
+var URL_PARAMS = encodeURI("utm_source=".concat(APP_NAME, "&utm_medium=referral"));
 
 var unsplash_PlaceholderImagesCollection = /*#__PURE__*/function () {
   function PlaceholderImagesCollection() {
@@ -21848,7 +21850,7 @@ var unsplash_PlaceholderImagesCollection = /*#__PURE__*/function () {
           }
         },
         title: photo.description,
-        caption: "<p class=\"credits hover\">Photo by <a href=\"".concat(photo.user.links.html, "\">").concat(photo.user.name, "</a> on <a href=\"https://unsplash.com\">Unsplash</a></p>"),
+        caption: "<p class=\"credits\">Photo by <a href=\"".concat(photo.user.links.html, "?").concat(URL_PARAMS, "\">").concat(photo.user.name, "</a> on <a href=\"https://unsplash.com?").concat(URL_PARAMS, "\">Unsplash</a></p>"),
         download: function download() {
           _this2.api.photos.downloadPhoto(photo);
         }
