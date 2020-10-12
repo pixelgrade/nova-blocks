400d49a8e6232dd7564998279ec7fec3d576ebab
madalingorbanescu <madalin@pixelgrade.com>
2020-10-12 11:50:50 +0300

    Add format library as package

--- packages/format-library/src/highlighted/index.js
+++ packages/format-library/src/highlighted/index.js
@@ -4,7 +4,7 @@
 const { __ } = wp.i18n;
 const { Fragment } = wp.element;
 const { toggleFormat } = wp.richText;
-const { RichTextToolbarButton, RichTextShortcut } = wp.blockEditor;
+const { RichTextToolbarButton } = wp.blockEditor;
 
 /**
  * Block constants
@@ -16,18 +16,12 @@ export const highlighted = {
 	title: __( 'Highlighted', 'nova-blocks' ),
 	tagName: 'span',
 	className: 'novablocks-highlighted',
-	attributes: {
-		style: 'style',
-	},
 	edit( { isActive, value, onChange } ) {
 
 		const onToggle = () => {
 			onChange(
 				toggleFormat( value, {
-					type: name,
-					attributes: {
-						style: 'background-color: var(----novablocks-color-1, #000); color: var(--novablocks-dark-1, #FFF); padding: 3px 6px;',
-					},
+					type: name
 				} )
 			);
 		};
@@ -39,7 +33,7 @@ export const highlighted = {
 					title={ __( 'Highlighted', 'nova-blocks' ) }
 					onClick={ onToggle }
 					isActive={ isActive }
-				/> )
+				/>
 			</Fragment>
 		);
 	},
