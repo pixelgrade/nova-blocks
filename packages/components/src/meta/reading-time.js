1bd0d67ed8beaeee5ae87dd14f6f1d7a6b3e471b
Mădălin Gorbănescu <46342490+madalingorbanescu@users.noreply.github.com>
2020-11-04 10:59:49 +0200

    Consider images and video when calculating reading time (#236)

--- packages/components/src/meta/reading-time.js
+++ packages/components/src/meta/reading-time.js
@@ -2,20 +2,41 @@ import { __, sprintf } from '@wordpress/i18n';
 
 const ReadingTime = ( props ) => {
 
-	let   wordsPerMinute = 265,
-		  wordsPerSecond = wordsPerMinute/ 60,
+	let   wordsPerMinute = 250,
+		  imgWeight = 12,
+		  imagesTime = 0,
+		  videoTime = 0,
 		  content = props.post?.content.rendered,
 		  strippedContent = content.replace( /(<([^>]+)>)/ig, ''),
 	      wordCount = strippedContent.split(' ')
 		                   .filter(function(n) { return n != '' })
 						   .length,
-		  seconds = Math.floor(wordCount/wordsPerSecond),
-		  minutes = Math.floor(wordCount/wordsPerMinute),
+		  words_time = Math.floor(wordCount / ( wordsPerMinute/60 ) );
 
-		  secondsRemainder = seconds % 60;
+	let matchImage = content.match(/<img\s[^>]+>/g);
+	let matchVideo = content.match(/<iframe\s[^>]+>/g);
 
-	if ( minutes < 1 || secondsRemainder > 40 ) {
-		minutes += 1;
+	if( matchImage !== null ) {
+
+		let numImages = matchImage.length;
+
+		for( let i = 0; i < numImages; i++) {
+			imagesTime += imgWeight;
+
+			if (imgWeight > 3) {
+				imgWeight--;
+			}
+		}
+	}
+
+	if( matchVideo !== null ) {
+		videoTime = matchVideo.length * 60;
+	}
+
+	let minutes = Math.ceil( ( words_time + imagesTime + videoTime ) / 60 );
+
+	if ( minutes < 1 ) {
+		minutes = 1;
 	}
 
 	return sprintf(__('%s min read', '__plugin_txtd'), minutes);
