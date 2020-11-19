99d51a91a73590deb9171ac879199783b3d7f356
Razvan <razwan.me@gmail.com>
2020-11-19 12:06:58 +0200

    Fix spacing inside block areas on pages that aren't built with the block editor

--- lib/client-assets.php
+++ lib/client-assets.php
@@ -431,7 +431,9 @@ function novablocks_dequeue_unused_block_assets() {
 	}
 
 	// Also dequeue the novablocks-core.
-	if ( ! is_admin() && is_singular() && ! has_blocks() ) {
+	$block_areas_have_blocks = novablocks_block_area_has_blocks( 'header' ) || ! novablocks_block_area_has_blocks( 'footer' ) || ! novablocks_block_area_has_blocks( 'promo-bar' );
+
+	if ( ! is_admin() && is_singular() && ! has_blocks() && ! $block_areas_have_blocks ) {
 		wp_dequeue_script( 'novablocks-core' );
 		wp_dequeue_style( 'novablocks-core-style' );
 	}
