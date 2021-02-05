fe5c10995cdd66a9b4ecc2eb124b2905386c0137
madalingorbanescu <madalin@pixelgrade.com>
2021-02-05 10:15:09 +0200

    Add helper class for primary row visibility on sticky header

--- packages/block-library/src/blocks/header/frontend.js
+++ packages/block-library/src/blocks/header/frontend.js
@@ -7,13 +7,14 @@ const HEADER_ROW_CLASS = '.site-header__row';
   function( $, window, undefined ) {
 
     const $siteHeader = $( '.site-header' ),
-      $stickyHeader = $( '.site-header-sticky' );
+          $stickyHeader = $( '.site-header-sticky' );
 
     let stickyRow = $siteHeader.data( 'sticky' ),
         stickyRowClass = (
           HEADER_ROW_CLASS + '--' + stickyRow
         ).toString(),
         stickyHeaderShown = false,
+        primaryRowShown = false,
         mainHeaderShouldBeSticky = $siteHeader.data( 'sticky' ) === 'primary' && !$stickyHeader.length,
         wpAdminBar = $('#wpadminbar'),
         wpAdminBarHeight = ! wpAdminBar.length ? '0' : wpAdminBar.outerHeight();
@@ -22,11 +23,15 @@ const HEADER_ROW_CLASS = '.site-header__row';
     $( window ).on( 'scroll', showStickyHeaderOnScroll );
     $( window ).on( 'scroll', makeHeaderStickyOnScroll );
 
+    // This function will add .social-menu-item class
+    // on menu-items if it's needed.
     addSocialMenuClass();
 
+    // We are using this function to display
+    // the newly created sticky header.
     function showStickyHeaderOnScroll() {
 
-      if ( stickyRow === 'none' || !$stickyHeader.length ) {
+      if ( stickyRow === 'none' || ! $stickyHeader.length ) {
         return;
       }
 
@@ -36,12 +41,23 @@ const HEADER_ROW_CLASS = '.site-header__row';
       }
 
       let stickyRowOffSet = $( stickyRowClass ).offset().top,
-        windowScrollY = window.scrollY,
-        isSticky = windowScrollY > stickyRowOffSet - wpAdminBarHeight,
-        $promoBar = $('.js-promo-bar');
+          windowScrollY = window.scrollY,
+          primaryRowIsVisible = windowScrollY > $siteHeader.outerHeight(),
+          isSticky = windowScrollY > stickyRowOffSet - wpAdminBarHeight,
+          $promoBar = $('.js-promo-bar');
 
+      // If we find any promo bar,
+      // we should consider it's height for the offset.
       if ( $promoBar.length ) {
         isSticky = windowScrollY > stickyRowOffSet - wpAdminBarHeight - $promoBar.outerHeight();
+      }
+
+      // Avoid showing primary row
+      // from sticky header on hover, when the primary row
+      // from main header is still visible.
+      if ( primaryRowIsVisible !==  primaryRowShown) {
+        $stickyHeader.toggleClass( 'primary-menu--is-visible' );
+        primaryRowShown = primaryRowIsVisible;
       }
 
       if ( isSticky !== stickyHeaderShown ) {
@@ -50,6 +66,8 @@ const HEADER_ROW_CLASS = '.site-header__row';
       }
     }
 
+    // We are using this function when
+    // header layout is simple (one-row).
     function makeHeaderStickyOnScroll() {
 
       // Do nothing if we are on mobile.
@@ -65,6 +83,5 @@ const HEADER_ROW_CLASS = '.site-header__row';
         stickyHeaderShown = mainHeaderIsSticky;
       }
     }
-
   }
 )( jQuery, window );
