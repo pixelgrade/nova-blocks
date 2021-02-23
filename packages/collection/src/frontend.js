c34a6b545108799dd9f15c980d6a8d15716be0fb
madalingorbanescu <madalin@pixelgrade.com>
2021-02-23 10:21:42 +0200

    Change arrow position and variable style on mobiles

--- packages/collection/src/frontend.js
+++ packages/collection/src/frontend.js
@@ -323,6 +323,7 @@ import {
               centerMode: true,
               infinite: true,
               slidesToShow: 1,
+              variableWidth: false
             }
           },
         ]
