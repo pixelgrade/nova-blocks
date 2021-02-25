e3de2a173480b2e87aa36c1d88fafe83c7483d2a
madalingorbanescu <madalin@pixelgrade.com>
2021-02-25 10:04:00 +0200

    Improve variation name

--- packages/block-library/src/blocks/posts-collection/variations.js
+++ packages/block-library/src/blocks/posts-collection/variations.js
@@ -3,7 +3,7 @@ import { __ } from '@wordpress/i18n';
 const variations = [
   {
     name: __('Posts Collection Carousel', '__plugin_txtd'),
-    title: __( 'Carousel', '__plugin_txtd' ),
+    title: __( 'Posts Carousel', '__plugin_txtd' ),
     description: __( 'Posts Collection displayed in a carousel layout', '__plugin_txtd' ),
     attributes: {
       layoutStyle:'carousel',
