b30b0fba448d9073ee7d315767d4328d62a83d53
madalingorbanescu <madalin@pixelgrade.com>
2021-04-12 10:26:14 +0300

    Cleanup in breaking alignemnts

--- packages/block-library/src/blocks/sidecar/frontend.js
+++ packages/block-library/src/blocks/sidecar/frontend.js
@@ -1,42 +1,6 @@
 const gridList = document.querySelectorAll(".site-main");
 const sidecars = document.querySelectorAll(".novablocks-sidecar");
 
-gridList.forEach(grid => {
-  // toggleOverlapClass(grid, ".alignfull:not(.novablocks-sidecar)", ".novablocks-sidecar--sidebar-left .novablocks-sidebar > *", "break-left");
-  // toggleOverlapClass(grid, ".alignfull:not(.novablocks-sidecar)", ".novablocks-sidecar:not(.novablocks-sidecar--sidebar-left) .novablocks-sidebar > *", "break-right");
-  // toggleOverlapClass(grid, ".alignwide:not(.novablocks-sidecar)", ".novablocks-sidecar--sidebar-left .novablocks-sidebar > *", "break-left");
-  // toggleOverlapClass(grid, ".alignwide:not(.novablocks-sidecar)", ".novablocks-sidecar:not(.novablocks-sidecar--sidebar-left) .novablocks-sidebar > *", "break-right");
-});
-
-function toggleOverlapClass(
-  container,
-  selector,
-  collisionSelector,
-  noCollisionClass
-) {
-  const elemList = container.querySelectorAll(selector);
-  const collidersList = container.querySelectorAll(collisionSelector);
-
-
-  elemList.forEach((elem) => {
-    const overlap = doesOverlap( elem, collidersList ) ;
-
-    if ( overlap ) {
-      elem.classList.add( noCollisionClass );
-    }
-  });
-}
-
-function doesOverlap( elem, collider ) {
-  const elemBox = elem.getBoundingClientRect();
-  const colliderBox = collider.getBoundingClientRect();
-
-  return ! (
-    colliderBox.top + colliderBox.height <= elemBox.top ||
-    colliderBox.top >= elemBox.top + elemBox.height
-  );
-}
-
 sidecars.forEach( sidecar => {
   let content = sidecar.querySelector(".novablocks-content"),
       sidebar = sidecar.querySelector(".novablocks-sidebar"),
@@ -53,3 +17,16 @@ sidecars.forEach( sidecar => {
     }
   } ) 
 })
+
+// Helper function to check
+// if two elements are overlapping
+function doesOverlap( elem, collider ) {
+
+  const elemBox = elem.getBoundingClientRect();
+  const colliderBox = collider.getBoundingClientRect();
+
+  return ! (
+    colliderBox.top + colliderBox.height <= elemBox.top ||
+    colliderBox.top >= elemBox.top + elemBox.height
+  );
+}
