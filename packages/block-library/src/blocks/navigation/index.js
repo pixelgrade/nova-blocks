d87e39f4bd89c93fbcad770eaff142e27dc03d71
madalingorbanescu <madalin@pixelgrade.com>
2021-07-21 16:26:37 +0300

    Select Header Row when selecting Logo or Navigation

--- packages/block-library/src/blocks/navigation/index.js
+++ packages/block-library/src/blocks/navigation/index.js
@@ -11,6 +11,11 @@ import { __ } from '@wordpress/i18n';
 import { registerBlockType } from '@wordpress/blocks';
 import { getSvg } from "@novablocks/block-editor";
 
+import {dispatch, select} from "@wordpress/data";
+
+const {getBlockRootClientId} = select( 'core/block-editor' );
+const {selectBlock, clearSelectedBlock} = dispatch( 'core/editor' );
+
 registerBlockType( 'novablocks/navigation', {
 	title: __( 'Space Navigation', '__plugin_txtd' ),
 	description: __( 'Outputs chosen navigation menu markup.', '__plugin_txtd' ),
@@ -18,12 +23,26 @@ registerBlockType( 'novablocks/navigation', {
   icon: getSvg( iconSvg ),
 	// Additional search terms
 	keywords: [ __( 'menu', '__plugin_txtd' ), __( 'site menu', '__plugin_txtd' ), __( 'primary', '__plugin_txtd' ), __( 'secondary', '__plugin_txtd' ) ],
-	parent: ['novablocks/header'],
+	parent: ['novablocks/header-row'],
 	attributes,
   supports: {
     html: false
   },
 	edit: function( props ) {
+
+    const {
+      clientId,
+      isSelected
+    } = props;
+
+    const parentClientId = getBlockRootClientId(clientId);
+
+    if ( isSelected ) {
+      clearSelectedBlock().then(() => {
+        selectBlock( parentClientId );
+      });
+    }
+
 		return (
 			<wp.serverSideRender
 				block="novablocks/navigation"
