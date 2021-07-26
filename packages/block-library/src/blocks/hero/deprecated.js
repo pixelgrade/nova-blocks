ccc4f7738960bbe7a5f5d3c628792c872bf550c1
madalingorbanescu <madalin@pixelgrade.com>
2021-07-26 14:15:14 +0300

    Split deprecated hero deprecated in two functions

--- packages/block-library/src/blocks/hero/deprecated.js
+++ packages/block-library/src/blocks/hero/deprecated.js
@@ -5,6 +5,95 @@ import { omit } from 'lodash';
 
 import save from "./save";
 
+export const migrateContentPadding = (attributes) => {
+
+  return {
+    attributes: {
+      ...attributes,
+      contentPadding: {
+        type: 'string',
+        default: 'small'
+      },
+      contentPaddingCustom: {
+        type: 'number',
+        default: 20
+      }
+    },
+    isEligible( attributes ) {
+      return attributes?.contentPadding === 'large' || attributes?.contentPadding === 'custom';
+    },
+    migrate( attributes ) {
+
+      const newAttributes = {};
+
+      if ( attributes?.contentPadding === 'large' ) {
+        newAttributes.emphasisTopSpacing = 2;
+        newAttributes.emphasisBottomSpacing = 2;
+      }
+
+      if ( attributes?.contentPadding === 'custom' ) {
+
+        if ( attributes?.contentPaddingCustom > 10 ) {
+          newAttributes.emphasisTopSpacing = 2;
+          newAttributes.emphasisBottomSpacing = 2;
+        }
+
+        if ( attributes?.contentPaddingCustom > 15 ) {
+          newAttributes.emphasisTopSpacing = 3;
+          newAttributes.emphasisBottomSpacing = 3;
+        }
+      }
+
+      return {
+        ...omit( attributes, [ 'contentPadding', 'contentPaddingCustom' ] ),
+        ...newAttributes
+      }
+    },
+  }
+}
+const migrateContentToGroup = (attributes) => {
+  return {
+    attributes: {
+      ...attributes,
+      contentWidth: {
+        type: 'string',
+        default: 'large'
+      },
+    },
+
+    isEligible( attributes, innerBlocks ) {
+      return innerBlocks[0].name !== 'core/group';
+    },
+
+    migrate( attributes, innerBlocks ) {
+
+      let groupBlockAlign = '';
+
+      if ( attributes?.contentWidth === 'full' ) {
+        groupBlockAlign = 'full';
+      }
+
+      if ( attributes?.contentWidth === 'large' ) {
+        groupBlockAlign = 'wide';
+      }
+
+      return [
+        attributes,
+        [
+          createBlock(
+            'core/group',
+            {
+              align: groupBlockAlign,
+            },
+            innerBlocks
+          )
+        ]
+      ]
+    },
+    save
+  }
+}
+
 const heroAddDeprecated = (settings, name) => {
 
   if ( name !== 'novablocks/hero' ) {
@@ -14,90 +103,8 @@ const heroAddDeprecated = (settings, name) => {
   const attributes = settings.attributes;
 
   const deprecated = [
-    {
-      attributes: {
-        ...attributes,
-        contentPadding: {
-          type: 'string',
-          default: 'small'
-        },
-        contentPaddingCustom: {
-          type: 'number',
-          default: 20
-        }
-      },
-      isEligible( attributes ) {
-        return attributes?.contentPadding === 'large' || attributes?.contentPadding === 'custom';
-      },
-      migrate( attributes ) {
-
-        const newAttributes = {};
-
-        if ( attributes?.contentPadding === 'large' ) {
-          newAttributes.emphasisTopSpacing = 2;
-          newAttributes.emphasisBottomSpacing = 2;
-        }
-
-        if ( attributes?.contentPadding === 'custom' ) {
-
-          if ( attributes?.contentPaddingCustom > 10 ) {
-            newAttributes.emphasisTopSpacing = 2;
-            newAttributes.emphasisBottomSpacing = 2;
-          }
-
-          if ( attributes?.contentPaddingCustom > 15 ) {
-            newAttributes.emphasisTopSpacing = 3;
-            newAttributes.emphasisBottomSpacing = 3;
-          }
-        }
-
-        return {
-          ...omit( attributes, [ 'contentPadding', 'contentPaddingCustom' ] ),
-          ...newAttributes
-        }
-      },
-      save
-    },
-    {
-      attributes: {
-        ...attributes,
-        contentWidth: {
-          type: 'string',
-          default: 'large'
-        },
-      },
-
-      isEligible( attributes, innerBlocks ) {
-        return innerBlocks[0].name !== 'core/group';
-      },
-
-      migrate( attributes, innerBlocks ) {
-
-        let groupBlockAlign = '';
-
-        if ( attributes?.contentWidth === 'full' ) {
-          groupBlockAlign = 'full';
-        }
-
-        if ( attributes?.contentWidth === 'large' ) {
-          groupBlockAlign = 'wide';
-        }
-
-        return [
-          attributes,
-          [
-            createBlock(
-              'core/group',
-              {
-                align: groupBlockAlign,
-              },
-              innerBlocks
-            )
-          ]
-        ]
-      },
-      save
-    }
+    migrateContentPadding( attributes),
+    migrateContentToGroup( attributes )
   ]
 
   return {
--- packages/block-library/src/blocks/slideshow/deprecated.js
+++ packages/block-library/src/blocks/slideshow/deprecated.js
@@ -1,9 +1,8 @@
-import { addFilter } from "@wordpress/hooks";
-import { omit } from 'lodash';
+import {addFilter} from "@wordpress/hooks";
 
-import save from "./save";
+import {migrateContentPadding} from '../hero/deprecated';
 
-const slideshowAddDeprecated = (settings, name) => {
+const slideshowAddDeprecated = ( settings, name ) => {
 
   if ( name !== 'novablocks/slideshow' ) {
     return settings;
@@ -12,50 +11,7 @@ const slideshowAddDeprecated = (settings, name) => {
   const attributes = settings.attributes;
 
   const deprecated = [
-    {
-      attributes: {
-        ...attributes,
-        contentPadding: {
-          type: 'string',
-          default: 'small'
-        },
-        contentPaddingCustom: {
-          type: 'number',
-          default: 20
-        }
-      },
-      isEligible( attributes ) {
-        return attributes?.contentPadding === 'large' || attributes?.contentPadding === 'custom';
-      },
-      migrate( attributes ) {
-
-        const newAttributes = {};
-
-        if ( attributes?.contentPadding === 'large' ) {
-          newAttributes.emphasisTopSpacing = 2;
-          newAttributes.emphasisBottomSpacing = 2;
-        }
-
-        if ( attributes?.contentPadding === 'custom' ) {
-
-          if ( attributes?.contentPaddingCustom > 10 ) {
-            newAttributes.emphasisTopSpacing = 2;
-            newAttributes.emphasisBottomSpacing = 2;
-          }
-
-          if ( attributes?.contentPaddingCustom > 15 ) {
-            newAttributes.emphasisTopSpacing = 3;
-            newAttributes.emphasisBottomSpacing = 3;
-          }
-        }
-
-        return {
-          ...omit( attributes, [ 'contentPadding', 'contentPaddingCustom' ] ),
-          ...newAttributes
-        }
-      },
-      save
-    },
+    migrateContentPadding( attributes )
   ]
 
   return {
@@ -64,4 +20,4 @@ const slideshowAddDeprecated = (settings, name) => {
   }
 }
 
-addFilter('blocks.registerBlockType', 'novablocks/slideshow-add-deprecated', slideshowAddDeprecated, 20);
+addFilter( 'blocks.registerBlockType', 'novablocks/slideshow-add-deprecated', slideshowAddDeprecated, 20 );
