b3cbd9ab4a2b9e61ac203896067f80a2207877c2
Razvan <razwan.me@gmail.com>
2021-08-04 17:00:14 +0300

    Add BlockAlignmentMatrixControl fallback

--- packages/block-editor/src/hooks/with-content-position-matrix/controls.js
+++ packages/block-editor/src/hooks/with-content-position-matrix/controls.js
@@ -1,7 +1,8 @@
 import { __ } from '@wordpress/i18n';
 import { BlockControls } from '@wordpress/block-editor';
 
-const BlockAlignmentMatrixControl = wp.blockEditor.__experimentalBlockAlignmentMatrixControl;
+const BlockAlignmentMatrixToolbar = wp.blockEditor.__experimentalBlockAlignmentMatrixToolbar;
+const BlockAlignmentMatrixControl = wp.blockEditor.__experimentalBlockAlignmentMatrixControl || wp.blockEditor.BlockAlignmentMatrixControl;
 
 const Controls = ( props ) => {
 
@@ -13,12 +14,40 @@ const Controls = ( props ) => {
   } = props;
 
   if ( ! BlockAlignmentMatrixControl ) {
-    return null;
+    return (
+      <ControlsFallback { ...props } />
+    );
   }
 
   return (
     <BlockControls group={ "block" }>
       <BlockAlignmentMatrixControl
+        label={ __( 'Change content position' ) }
+        value={ contentPosition }
+        onChange={ ( nextPosition ) =>
+          setAttributes( { contentPosition: nextPosition } )
+        }
+      />
+    </BlockControls>
+  )
+}
+
+const ControlsFallback = ( props ) => {
+
+  const {
+    attributes: {
+      contentPosition,
+    },
+    setAttributes,
+  } = props;
+
+  if ( ! BlockAlignmentMatrixToolbar ) {
+    return null;
+  }
+
+  return (
+    <BlockControls>
+      <BlockAlignmentMatrixToolbar
         label={ __( 'Change content position' ) }
         value={ contentPosition }
         onChange={ ( nextPosition ) =>
