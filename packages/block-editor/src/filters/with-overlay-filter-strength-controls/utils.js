76b9f8c8c7bf341b00f53e7dd57ed0be1400ab93
madalingorbanescu <madalin@pixelgrade.com>
2021-09-03 09:32:24 +0300

    Small formatting

--- packages/block-editor/src/filters/with-overlay-filter-strength-controls/utils.js
+++ packages/block-editor/src/filters/with-overlay-filter-strength-controls/utils.js
@@ -1,11 +1,9 @@
-
 /**
  * Returns a Duotone Presets Array.
  * @param palettes
  * @returns {array}
  */
 export const generateDuotonePresetsFromPalettes = ( palettes ) => {
-
   // First number is used for highlights.
   // Second number is used for shadows.
   // Since JavaScript arrays are zero-indexed,
@@ -33,37 +31,34 @@ export const generateDuotonePresetsFromPalettes = ( palettes ) => {
     [7, 8],
     [7, 9],
     [7, 10],
-    [7, 11]
+    [7, 11],
   ];
 
   const presets = [];
 
   duotones.forEach( ( [highlightIndex, shadowIndex] ) => {
+    highlightIndex = clamp( highlightIndex, 0, 11 );
+    shadowIndex = clamp( shadowIndex, 0, 11 );
 
-    highlightIndex = clamp(highlightIndex, 0, 11);
-    shadowIndex = clamp(shadowIndex, 0, 11);
-
-    palettes.forEach( palette1 => {
-      palettes.forEach( palette2 => {
-
+    palettes.forEach( ( palette1 ) => {
+      palettes.forEach( ( palette2 ) => {
         if ( palette2.id === palette1.id ) {
           return;
         }
 
-        const highlightsColor = palette1.colors[highlightIndex]['value'];
-        const shadowsColor = palette2.colors[shadowIndex]['value'];
+        const highlightsColor = palette1.colors[highlightIndex]["value"];
+        const shadowsColor = palette2.colors[shadowIndex]["value"];
 
         presets.push( {
           name: `${palette2.label}(${shadowIndex}) and ${palette1.label}(${highlightIndex})`,
-          colors: [shadowsColor, highlightsColor]
-        } )
-
+          colors: [shadowsColor, highlightsColor],
+        } );
       } );
     } );
-  } )
+  } );
 
   return presets;
-}
+};
 
 /**
  * Returns all color from Palettes as array.
@@ -72,30 +67,27 @@ export const generateDuotonePresetsFromPalettes = ( palettes ) => {
  */
 
 export const generateColorPalettes = ( palettes ) => {
-
-  let colorItem = {}
-  const colorPalette = []
+  let colorItem = {};
+  const colorPalette = [];
 
   palettes.forEach( ( palette, index ) => {
-
     let colors = palette.colors;
     let colorLabel = palette.label;
 
-    colors.forEach( (color, colorIndex) => {
-
+    colors.forEach( ( color, colorIndex ) => {
       colorItem = {
-        'color': color.value,
-        'name': `${colorLabel} - Color-${colorIndex}`,
-        'slug': `${colorLabel.toLowerCase().replace( /\s/g, '-' )}-color-${index}`
-      }
+        color: color.value,
+        name: `${colorLabel} - Color-${colorIndex}`,
+        slug: `${colorLabel.toLowerCase().replace( /\s/g, "-" )}-color-${index}`,
+      };
 
-      colorPalette.push( colorItem )
-    } )
+      colorPalette.push( colorItem );
+    } );
   } );
 
   return colorPalette;
-}
+};
 
 const clamp = ( number, min, max ) => {
-  return Math.min( Math.max( min, number ), max )
+  return Math.min( Math.max( min, number ), max );
 };
