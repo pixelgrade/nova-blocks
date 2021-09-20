e44dad8e4b37807a28bc69ef6e8372d1c7190eb0
madalingorbanescu <madalin@pixelgrade.com>
2021-09-20 15:22:23 +0300

    Remove garbage if

--- packages/block-editor/src/filters/with-emphasis-control/with-emphasis-area-edit-custom-props.js
+++ packages/block-editor/src/filters/with-emphasis-control/with-emphasis-area-edit-custom-props.js
@@ -1,24 +1,18 @@
 import {createHigherOrderComponent} from "@wordpress/compose";
-import {useSupports} from "../../hooks";
 
 const withEmphasisAreaEditCustomProps = createHigherOrderComponent( OriginalComponent => {
 
   return ( props ) => {
 
-    const supports = useSupports( props.name );
-    const colorSignalSupport = supports?.novaBlocks?.colorSignal;
     const {attributes} = props;
 
     const {emphasisArea} = attributes;
 
     const style = props.style ? props.style : {};
 
-    if ( colorSignalSupport === true ) {
+    let colorSignalProps = {'--nb-collection-emphasis-area': emphasisArea}
 
-      let colorSignalProps = {'--nb-collection-emphasis-area': emphasisArea,}
-
-      Object.assign( style, colorSignalProps );
-    }
+    Object.assign( style, colorSignalProps );
 
     return (
       <OriginalComponent {...props} style={style}/>
