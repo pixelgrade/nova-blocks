4c99e5c90143ef9e1d8f4ce1fbd751f9de82be0a
madalingorbanescu <madalin@pixelgrade.com>
2021-09-22 12:03:52 +0300

    Slider color should always match text color related to #357

--- packages/block-library/src/blocks/supernova/components/carousel-layout/index.js
+++ packages/block-library/src/blocks/supernova/components/carousel-layout/index.js
@@ -1,21 +1,51 @@
 import Slider from "react-slick";
 
-const CarouselLayout = ( props ) => {
+const SliderArrow = ( props ) => {
 
   const {
-    attributes: {
-      columns,
-      carouselLayout,
-      showArrows,
-      showPagination,
-    },
+    attributes,
+    onClick,
+    isNext
   } = props;
+
+  const {
+    palette,
+    contentPaletteVariation,
+    contentColorSignal
+  } = attributes;
+
+  const PALETTE_CLASS = `sm-palette-${palette}`;
+  const PALETTE_VARIATION_CLASS = `sm-variation-${contentPaletteVariation}`;
+  const CONTENT_SIGNAL_CLASS = `sm-color-signal-${contentColorSignal}`;
+
+  let BUTTON_LABEL = isNext ? 'Next' : 'Previous'
+  let BUTTON_DIRECTION_CLASS = isNext ? 'slick-next' : 'slick-prev'
+
+  return (
+    <button
+      className={`slick-arrow ${BUTTON_DIRECTION_CLASS} ${PALETTE_CLASS} ${PALETTE_VARIATION_CLASS} ${CONTENT_SIGNAL_CLASS}`}
+      aria-label={BUTTON_LABEL} onClick={onClick}>{BUTTON_LABEL}</button>
+  )
+}
+
+const CarouselLayout = ( props ) => {
+
+  const { attributes } = props;
+
+  const {
+    columns,
+    carouselLayout,
+    showArrows,
+    showPagination
+  } = attributes;
 
   const settings = {
     arrows: showArrows,
     dots: showPagination,
     infinite: true,
     variableWidth: carouselLayout === 'variable' || carouselLayout === 'content',
+    prevArrow: <SliderArrow {...props} />,
+    nextArrow: <SliderArrow isNext={true} {...props}  />,
   };
 
   if ( carouselLayout !== 'variable' && carouselLayout !== 'content' ) {
@@ -23,7 +53,7 @@ const CarouselLayout = ( props ) => {
   }
 
   return (
-    <Slider { ...settings }>{ props.children }</Slider>
+    <Slider {...settings}>{props.children}</Slider>
   );
 }
 
