816a30d51f93d9cb76cb8f6ac55fb157051b01a8
Razvan <razwan.me@gmail.com>
2021-12-02 12:45:05 +0200

    Quickfix media composition output

--- packages/block-library/src/blocks/supernova-item/init.php
+++ packages/block-library/src/blocks/supernova-item/init.php
@@ -44,6 +44,6 @@ if ( ! function_exists( 'novablocks_render_supernova_item_block' ) ) {
 			$card_content = novablocks_get_card_contents( $attributes );
 		}
 
-		return novablocks_get_collection_card_markup( $card_media, $card_content, $attributes );
+		return novablocks_get_collection_card_markup( $media_markup, $card_content, $attributes );
 	}
 }
