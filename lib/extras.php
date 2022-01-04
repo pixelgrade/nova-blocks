93502911c3ff2cfb0a71c90159603d0b62867b26
George Olaru <george@pixelgrade.com>
2022-01-04 15:45:41 +0200

    Block Patterns: added two Headlines patterns

--- lib/extras.php
+++ lib/extras.php
@@ -2524,13 +2524,14 @@ function novablocks_get_tag_name( $tag ) {
 /**
  * BLOCK PATTERNS
  *
- * #PRESENTATION
- * Headlines..............Hero banners, website intros, slideshows, CTAs, Media Cards → Hero
+ * #PRESENTATION — Visual storytelling
+ * Headlines..............Hero banners, website intros, slideshows, CTAs, Media Cards → Hero, Image with text, Featuring
  * Features...............A list features for a service or product
  * Team...................Lists of team members
  * Testimonials...........Reviews
  * Gallery................Portfolio
  * Video..................Presentation blocks that include videos (Hero, one and two columns)
+ *
  * Lists..................Various types of lists (eg. Features, Steps)
  *
  * #UTILITY
@@ -2559,6 +2560,11 @@ function novablocks_register_block_patterns() {
 	);
 
 	register_block_pattern_category(
+		'novablocks/headlines',
+		array( 'label' => __( 'Headlines', '__plugin_txtd' ) )
+	);
+
+	register_block_pattern_category(
 		'novablocks/testimonials',
 		array( 'label' => __( 'Testimonials', '__plugin_txtd' ) )
 	);
@@ -2574,6 +2580,90 @@ function novablocks_register_block_patterns() {
 	);
 
 
+
+
+	/*------------------------------------*\
+	  HEADLINES
+	\*------------------------------------*/
+
+	register_block_pattern(
+		'novablocks/headline-1',
+		array(
+			'title'       => __( 'Feature with Testimonial', '__plugin_txtd' ),
+			'description' => _x( '...', 'Block pattern description', '__plugin_txtd' ),
+			'content'     => '<!-- wp:novablocks/supernova {"variation":"media-card","sourceType":"blocks","postsToShow":1,"showCollectionTitle":false,"showCollectionSubtitle":false,"contentPosition":"center left","cardLayout":"horizontal-reverse","layoutStyle":"classic","thumbnailAspectRatio":40,"blockTopSpacing":0,"blockBottomSpacing":1,"emphasisBottomSpacing":1,"contentAreaWidth":45,"layoutGutter":100} -->
+							<!-- wp:novablocks/supernova-item {"defaultsGenerated":true,"sourceType":"blocks","postsToShow":1,"showCollectionTitle":false,"showCollectionSubtitle":false,"contentPosition":"center left","cardLayout":"horizontal-reverse","layoutStyle":"classic","thumbnailAspectRatio":40,"blockTopSpacing":0,"blockBottomSpacing":1,"emphasisBottomSpacing":1,"contentAreaWidth":45,"layoutGutter":100,"images":[{"sizes":{"thumbnail":{"height":150,"width":150,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-150x150.jpg","orientation":"landscape"},"medium":{"height":238,"width":300,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-300x238.jpg","orientation":"landscape"},"large":{"height":811,"width":1024,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-1024x811.jpg","orientation":"landscape"},"novablocks_large":{"height":1082,"width":1366,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-1366x1082.jpg","orientation":"landscape"},"novablocks_medium":{"height":811,"width":1024,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-1024x811.jpg","orientation":"landscape"},"novablocks_small":{"height":608,"width":768,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-768x608.jpg","orientation":"landscape"},"novablocks_tiny":{"height":380,"width":480,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut-480x380.jpg","orientation":"landscape"},"full":{"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut.jpg","height":1419,"width":1791,"orientation":"landscape"}},"mime":"image/jpeg","type":"image","subtype":"jpeg","id":3795,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/nova-astronaut.jpg","alt":"","link":"https://trial.pixelgrade.com/tosca/block-patterns/headlines/nova-astronaut/","caption":"","description":""}],"stylePreset":"just-my-style","sizeContrast":80,"positionShift":95,"elementsDistance":80,"placementVariation":50} -->
+							<!-- wp:heading {"className":"has-larger-font-size","fontSize":"larger"} -->
+							<h2 class="has-larger-font-size"><mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-black-color">***</mark></h2>
+							<!-- /wp:heading -->
+
+							<!-- wp:heading {"level":1,"className":"has-smaller-font-size","fontSize":"smaller"} -->
+							<h1 class="has-smaller-font-size" id="welcome-to-our-galaxy-we-ve-got-a-new-look">Welcome to our galaxy, we’ve got a new look!</h1>
+							<!-- /wp:heading -->
+
+							<!-- wp:paragraph {"className":"is-style-lead"} -->
+							<p class="is-style-lead has-normal-font-size">We\'ve always defined ourselves by the ability to overcome the impossible. And we count these moments.</p>
+							<!-- /wp:paragraph -->
+
+							<!-- wp:buttons -->
+							<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-primary"} -->
+							<div class="wp-block-button is-style-primary"><a class="wp-block-button__link">Learn more</a></div>
+							<!-- /wp:button --></div>
+							<!-- /wp:buttons -->
+
+							<!-- wp:separator {"className":"is-style-simple","blockTopSpacing":2,"blockBottomSpacing":1} -->
+							<div class="wp-block-separator alignnone is-style-simple" style="--nb-emphasis-top-spacing:0;--nb-emphasis-bottom-spacing:0;--nb-block-top-spacing:2;--nb-block-bottom-spacing:1;--nb-block-zindex:0;--nb-card-content-area-width:50%;--nb-card-content-padding-multiplier:0;--nb-card-media-padding-top:100%;--nb-card-media-object-fit:cover;--nb-card-media-padding-multiplier:0;--nb-card-layout-gap-modifier:0;--nb-minimum-container-height:0vh;--nb-spacing-modifier:1">
+							        <div class="c-separator">
+							            <div class="c-separator__arrow c-separator__arrow--left"></div>
+							            <div class="c-separator__line c-separator__line--left"></div>
+							            <div class="c-separator__symbol">
+							                <span><svg width="19" height="20" viewBox="0 0 19 20" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.7565 8.5753c1.3088-1.6632 2.3177-2.813 3.0266-3.4492.718-.6453 1.3315-.968 1.8405-.968.4181 0 .7817.1682 1.0907.5045.309.3363.4635.7362.4635 1.1997 0 .6726-.4453 1.2043-1.3361 1.5951-.8907.3908-2.5857.7635-5.0852 1.118zm-2.454-1.4315c-.518-1.2633-.918-2.3585-1.1997-3.2856-.2817-.927-.4226-1.6269-.4226-2.0995 0-.5544.1409-.9861.4226-1.2952C8.3846.1545 8.7754 0 9.2753 0c.518 0 .918.1545 1.1997.4635.2818.309.4226.7408.4226 1.2952 0 .4544-.1408 1.1543-.4226 2.0995-.2727.9362-.6635 2.0314-1.1724 3.2856zm2.454 4.2809c2.4995.3544 4.1945.7271 5.0852 1.1179.8908.3908 1.3361.918 1.3361 1.5815 0 .4726-.1545.877-.4635 1.2133-.309.3272-.6726.4908-1.0907.4908-.509 0-1.1225-.3181-1.8405-.9543-.7089-.6362-1.7178-1.786-3.0266-3.4492zm-.0545-1.4315c0 .6635-.2363 1.2315-.7089 1.7041-.4636.4727-1.027.709-1.6905.709-.6726 0-1.2452-.2318-1.7178-.6953-.4727-.4726-.709-1.0452-.709-1.7178 0-.6544.2363-1.218.709-1.6905.4726-.4727 1.036-.709 1.6905-.709.6635 0 1.2315.2363 1.7041.709.4817.4726.7226 1.036.7226 1.6905zM6.8213 8.5753c-2.4994-.3544-4.1945-.727-5.0852-1.1179C.8454 7.0666.4 6.5349.4 5.8623c0-.4635.1544-.8634.4635-1.1997.309-.3363.6725-.5045 1.0906-.5045.509 0 1.118.3227 1.8269.968.7089.6362 1.7223 1.786 3.0402 3.4492zm2.454 4.2672c.518 1.2725.918 2.3722 1.1997 3.2993.2818.927.4226 1.6269.4226 2.0995 0 .5544-.1408.9861-.4226 1.2952-.2817.309-.6726.4635-1.1724.4635-.5181 0-.918-.1545-1.1998-.4635-.2817-.3091-.4226-.7408-.4226-1.2952 0-.4635.1363-1.1588.409-2.0859.2727-.927.668-2.0313 1.186-3.3129zm-2.454-1.4178c-1.336 1.6996-2.354 2.8584-3.0539 3.4765-.6998.618-1.3042.927-1.8132.927a1.383 1.383 0 0 1-.709-.1908c-.218-.1364-.427-.35-.627-.6408-.0728-.1727-.1273-.3272-.1636-.4635a1.582 1.582 0 0 1-.0546-.409c0-.6635.4454-1.1907 1.336-1.5815.8908-.3908 2.5859-.7635 5.0853-1.1179z" fill="currentColor"/></svg>
+							</span>
+							            </div>
+							            <div class="c-separator__line c-separator__line--right"></div>
+							            <div class="c-separator__arrow c-separator__arrow--right"></div>
+							        </div>
+									</div>
+							<!-- /wp:separator -->
+
+							<!-- wp:pullquote -->
+							<figure class="wp-block-pullquote has-normal-font-size"><blockquote><p>“As a visual person, I couldn’t resist Pixelgrade’s design oriented solutions. Their unique aesthetics make them really special and stand out from the crowd.”</p><cite>— Anne Marie, Journalist from Netherlands</cite></blockquote></figure>
+							<!-- /wp:pullquote -->
+							<!-- /wp:novablocks/supernova-item -->
+							<!-- /wp:novablocks/supernova -->',
+			'categories'  => array( 'novablocks/headlines' )
+		)
+	);
+
+	register_block_pattern(
+		'novablocks/headline-2',
+		array(
+			'title'       => __( 'Simple Feature', '__plugin_txtd' ),
+			'description' => _x( '...', 'Block pattern description', '__plugin_txtd' ),
+			'content'     => '<!-- wp:novablocks/supernova {"variation":"media-card","sourceType":"blocks","postsToShow":1,"showCollectionTitle":false,"showCollectionSubtitle":false,"contentPosition":"center left","cardLayout":"horizontal","layoutStyle":"classic","thumbnailAspectRatio":40,"contentPadding":50,"blockTopSpacing":0,"blockBottomSpacing":1,"emphasisBottomSpacing":1,"contentAreaWidth":45,"layoutGutter":100,"objectPosition":30} -->
+						<!-- wp:novablocks/supernova-item {"defaultsGenerated":true,"sourceType":"blocks","postsToShow":1,"showCollectionTitle":false,"showCollectionSubtitle":false,"contentPosition":"center left","cardLayout":"horizontal","layoutStyle":"classic","thumbnailAspectRatio":40,"contentPadding":50,"blockTopSpacing":0,"blockBottomSpacing":1,"emphasisBottomSpacing":1,"contentAreaWidth":45,"layoutGutter":100,"images":[{"sizes":{"thumbnail":{"height":150,"width":150,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-150x150.jpg","orientation":"landscape"},"medium":{"height":300,"width":225,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-225x300.jpg","orientation":"portrait"},"large":{"height":1024,"width":767,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-767x1024.jpg","orientation":"portrait"},"novablocks_large":{"height":1366,"width":1024,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-1024x1366.jpg","orientation":"portrait"},"novablocks_medium":{"height":1024,"width":767,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-767x1024.jpg","orientation":"portrait"},"novablocks_small":{"height":768,"width":576,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-576x768.jpg","orientation":"portrait"},"novablocks_tiny":{"height":480,"width":360,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash-360x480.jpg","orientation":"portrait"},"full":{"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash.jpg","height":2000,"width":1499,"orientation":"portrait"}},"mime":"image/jpeg","type":"image","subtype":"jpeg","id":3819,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-994012-unsplash.jpg","alt":"","link":"https://trial.pixelgrade.com/tosca/block-patterns/headlines/annie-spratt-994012-unsplash/","caption":"","description":""},{"sizes":{"thumbnail":{"height":150,"width":150,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-150x150.jpg","orientation":"landscape"},"medium":{"height":300,"width":192,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-192x300.jpg","orientation":"portrait"},"large":{"height":1024,"width":654,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-654x1024.jpg","orientation":"portrait"},"novablocks_large":{"height":1366,"width":873,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-873x1366.jpg","orientation":"portrait"},"novablocks_medium":{"height":1024,"width":654,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-654x1024.jpg","orientation":"portrait"},"novablocks_small":{"height":768,"width":491,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-491x768.jpg","orientation":"portrait"},"novablocks_tiny":{"height":480,"width":307,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash-307x480.jpg","orientation":"portrait"},"full":{"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash.jpg","height":2000,"width":1278,"orientation":"portrait"}},"mime":"image/jpeg","type":"image","subtype":"jpeg","id":3817,"url":"https://trial.pixelgrade.com/tosca/wp-content/uploads/sites/3/2022/01/annie-spratt-973393-unsplash.jpg","alt":"","link":"https://trial.pixelgrade.com/tosca/block-patterns/headlines/annie-spratt-973393-unsplash/","caption":"","description":""}],"stylePreset":"just-my-style","objectPosition":30} -->
+						<!-- wp:heading {"level":4,"className":"has-smaller-font-size","fontSize":"smaller"} -->
+						<h4 class="has-smaller-font-size" id="shoot-for-the-moon-even-if-you-miss">Shoot for the moon, Even if You Miss.</h4>
+						<!-- /wp:heading -->
+
+						<!-- wp:heading {"level":1,"className":"has-smaller-font-size","fontSize":"smaller"} -->
+						<h1 class="has-smaller-font-size" id="welcome-to-our-galaxy-we-ve-got-a-new-look">Welcome to our planet, look and feel matters<em>!</em></h1>
+						<!-- /wp:heading -->
+
+						<!-- wp:paragraph {"className":"is-style-default"} -->
+						<p class="is-style-default has-normal-font-size">If you could see the earth illuminated when you were in a place as dark as night, it would look to you more splendid than the moon. I don\'t know what you could say about a day in which you have seen four beautiful sunsets.</p>
+						<!-- /wp:paragraph -->
+
+						<!-- wp:buttons -->
+						<div class="wp-block-buttons"><!-- wp:button {"className":"is-style-primary"} -->
+						<div class="wp-block-button is-style-primary"><a class="wp-block-button__link">Discover more</a></div>
+						<!-- /wp:button --></div>
+						<!-- /wp:buttons -->
+						<!-- /wp:novablocks/supernova-item -->
+						<!-- /wp:novablocks/supernova -->',
+			'categories'  => array( 'novablocks/headlines' )
+		)
+	);
 
 
 
