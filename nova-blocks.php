b51a7a832000b48df55ba1db4151a55910a4b6a4
Vlad Olaru <vlad@pixelgrade.com>
2022-01-28 12:33:25 +0200

    Bump tested up to and required WP version to 5.9

--- nova-blocks.php
+++ nova-blocks.php
@@ -9,7 +9,7 @@
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain: __plugin_txtd
- * Requires at least: 5.9.0
+ * Requires at least: 5.9
  * Tested up to: 5.9.0
  * Requires PHP: 7.1
  * GitHub Plugin URI: pixelgrade/nova-blocks
