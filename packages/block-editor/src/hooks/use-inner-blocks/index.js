e79dcd61ea1a4583ca26f2137aae4c21f0747fb3
Vlad Olaru <vlad@pixelgrade.com>
2022-02-17 16:58:47 +0200

    Fix error when deleting supernova

--- packages/block-editor/src/hooks/use-inner-blocks/index.js
+++ packages/block-editor/src/hooks/use-inner-blocks/index.js
@@ -1,7 +1,11 @@
 import { useSelect } from '@wordpress/data';
 
 const useInnerBlocks = ( clientId ) => {
-  return useSelect( select => select( 'core/block-editor' ).getBlock( clientId ).innerBlocks, [ clientId ] )
+  return useSelect( select => {
+      return select( 'core/block-editor' ).getBlock( clientId )?.innerBlocks || []
+    },
+    [ clientId ]
+  );
 };
 
 export default useInnerBlocks;
