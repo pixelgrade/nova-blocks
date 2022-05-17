800b004cd83f70f3890116f5f2a1fc35fb8b5fe8
Razvan <razwan.me@gmail.com>
2022-05-17 12:07:56 +0300

    Apply color signal to all scroll indicators inside heroes

--- packages/color-signal/src/frontend/update-scroll-indicator.js
+++ packages/color-signal/src/frontend/update-scroll-indicator.js
@@ -6,35 +6,38 @@ export const updateScrollIndicator = () => {
   const blocksArray = [ ...blocks ].filter( block => !! block.dataset.scrollIndicatorBlock );
 
   blocksArray.forEach( block => {
-    const indicator = block.querySelector( '.nb-scroll-indicator:not(.nb-scroll-indicator--middle)' );
+    const indicators = Array.from( block.querySelectorAll( '.nb-scroll-indicator:not(.nb-scroll-indicator--middle)' ) );
     const nextElement = block.nextElementSibling;
 
     if ( ! nextElement ) {
-      applySiteColorSignal( indicator );
+      indicators.forEach( applySiteColorSignal );
       return;
     }
 
     const nextElementStyles = window.getComputedStyle( nextElement );
     const marginTop = nextElementStyles.getPropertyValue( 'margin-top' );
 
-    if ( parseInt( marginTop, 10 ) !== 0 ) {
-      applySiteColorSignal( indicator );
-      return;
-    }
+    indicators.forEach( indicator => {
 
-    if ( !! indicator && !! nextElement?.dataset?.colorSignal ) {
-      const attributes = nextElement.dataset;
-      const classNamesToAdd = getColorSignalClassnames( attributes, true );
-      const classNamesToRemove = [ ...indicator.classList ].filter( className => {
-        return className.indexOf( 'sm-palette-' ) > -1 ||
-               className.indexOf( 'sm-variation-' ) > -1 ||
-               className.indexOf( 'sm-color-signal-' ) > -1;
-      } ).join( " " );
+      if ( parseInt( marginTop, 10 ) !== 0 ) {
+        applySiteColorSignal( indicator );
+        return;
+      }
 
-      removeClass( indicator, classNamesToRemove );
-      addClass( indicator, classNamesToAdd );
-    }
+      if ( !! indicator && !! nextElement?.dataset?.colorSignal ) {
+        const attributes = nextElement.dataset;
+        const classNamesToAdd = getColorSignalClassnames( attributes, true );
+        const classNamesToRemove = [ ...indicator.classList ].filter( className => {
+          return className.indexOf( 'sm-palette-' ) > -1 ||
+                 className.indexOf( 'sm-variation-' ) > -1 ||
+                 className.indexOf( 'sm-color-signal-' ) > -1;
+        } ).join( " " );
 
+        removeClass( indicator, classNamesToRemove );
+        addClass( indicator, classNamesToAdd );
+      }
+
+    } );
   } );
 };
 
