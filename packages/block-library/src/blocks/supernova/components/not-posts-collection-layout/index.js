402dc7cfc6af23e1b339310eadd2598030bfa28e
Razvan <razwan.me@gmail.com>
2022-07-06 11:10:57 +0300

    Fix adding placeholder images to supernova blocks that are initialized in preview mode

--- packages/block-library/src/blocks/supernova/components/not-posts-collection-layout/index.js
+++ packages/block-library/src/blocks/supernova/components/not-posts-collection-layout/index.js
@@ -1,9 +1,10 @@
-import { Fragment } from "@wordpress/element";
 import { useInnerBlocksProps } from "@wordpress/block-editor";
+import { useDispatch } from "@wordpress/data";
+import { useEffect, useRef } from "@wordpress/element";
 
-import { useInnerBlocks } from "@novablocks/block-editor";
+import { getPlaceholderImages, useInnerBlocks } from "@novablocks/block-editor";
 import { CollectionBody } from "@novablocks/collection";
-import { needsPreview } from "@novablocks/utils";
+import { getRandomArrayFromArray, needsPreview } from "@novablocks/utils";
 
 import { SupernovaItemPreview } from "../index";
 
@@ -35,8 +36,42 @@ const CardsCollectionPreview = ( props ) => {
   )
 };
 
+
+async function getRandomImages() {
+  const placeholderImages = await getPlaceholderImages();
+
+  return placeholderImages;
+}
+
 const CardsCollectionLayout = props => {
-  const { attributes } = props;
+  const { attributes, clientId } = props;
+  const innerBlocks = useInnerBlocks( clientId );
+  const allDefaultsGenerated = useRef( false );
+  const { updateBlockAttributes } = useDispatch( 'core/block-editor' );
+
+  useEffect( () => {
+
+    if ( ! allDefaultsGenerated.current ) {
+
+      if ( needsPreview( attributes ) ) {
+        const defaultsGenerated = innerBlocks.every( block => block.attributes.defaultsGenerated );
+
+        if ( ! defaultsGenerated ) {
+          getRandomImages().then( placeholderImages => {
+            innerBlocks.forEach( block => {
+              if ( ! block.attributes.defaultsGenerated ) {
+                const images = getRandomArrayFromArray( placeholderImages, 1 );
+                updateBlockAttributes( block.clientId, { defaultsGenerated: true, images } );
+              }
+            } );
+          } );
+        }
+      }
+
+      allDefaultsGenerated.current = true;
+    }
+
+  }, [ attributes, innerBlocks ] )
 
   if ( needsPreview( attributes ) && attributes.preview ) {
     return (
