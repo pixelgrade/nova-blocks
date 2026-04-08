4a404a443fc7b6913bdb7acab75182b1e412eea1
George Olaru <george@pixelgrade.com>
2026-04-08 16:13:59 +0300

    Preserve carousel unmasking with no block color signal
    Fixes #513

---
+++ packages/block-library/src/blocks/supernova/style.test.js
@@ -0,0 +1,14 @@
+const test = require('node:test');
+const assert = require('node:assert/strict');
+const fs = require('node:fs');
+const path = require('node:path');
+
+const stylePath = path.join(__dirname, 'style.scss');
+const styleSource = fs.readFileSync(stylePath, 'utf8');
+
+test('carousel blocks with no block color signal still mask stacked slide layers', () => {
+  assert.match(
+    styleSource,
+    /&\.nb-supernova--layout-carousel\.sm-color-signal-0\s+\.nb-supernova-item__frame\s*>\s*:is\(\.nb-supernova-item__content,\s*\.nb-supernova-item__media-wrapper\)\s*\{[\s\S]*?background-color:\s*var\(--sm-current-bg-color,\s*var\(--nb-bg-color\)\);/
+  );
+} );
