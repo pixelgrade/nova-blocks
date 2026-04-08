dbea06b9330463a3cc0bec93659910d186074234
George Olaru <george@pixelgrade.com>
2026-04-08 16:24:38 +0300

    Scope carousel masking to item color signal
    Fixes #514

--- packages/block-library/src/blocks/supernova/style.test.js
+++ packages/block-library/src/blocks/supernova/style.test.js
@@ -7,8 +7,13 @@ const stylePath = path.join(__dirname, 'style.scss');
 const styleSource = fs.readFileSync(stylePath, 'utf8');
 
 test('carousel blocks with no block color signal still mask stacked slide layers', () => {
+  assert.doesNotMatch(
+    styleSource,
+    /&\.nb-supernova--layout-carousel\.sm-color-signal-0\s+\.nb-supernova-item__frame\s*>\s*:is\(\.nb-supernova-item__content,\s*\.nb-supernova-item__media-wrapper\)\s*\{/
+  );
+
   assert.match(
     styleSource,
-    /&\.nb-supernova--layout-carousel\.sm-color-signal-0\s+\.nb-supernova-item__frame\s*>\s*:is\(\.nb-supernova-item__content,\s*\.nb-supernova-item__media-wrapper\)\s*\{[\s\S]*?background-color:\s*var\(--sm-current-bg-color,\s*var\(--nb-bg-color\)\);/
+    /&\.nb-supernova--layout-carousel\s+\.nb-supernova-item\.sm-color-signal-0\s+\.nb-supernova-item__frame\s*>\s*:is\(\.nb-supernova-item__content,\s*\.nb-supernova-item__media-wrapper\)\s*\{[\s\S]*?background-color:\s*var\(--sm-current-bg-color,\s*var\(--nb-bg-color\)\);/
   );
 } );
